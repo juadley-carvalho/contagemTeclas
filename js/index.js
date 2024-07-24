3859c31fecefc9b75923001d86216faea5c5ebb6
juadley <juadley.carvalho@hotmail.com>
2024-07-24 12:01:51 -0300

    v1.0.0 - Primeira versão para teclados abnt2 full size.

--- js/index.js
+++ js/index.js
@@ -11,12 +11,22 @@ async function loadJson() {
 
 // Função para calcular a cor baseada na frequência
 function getColor(frequency) {
-    const maxFrequency = 500; // Suponha que 100 seja a frequência máxima para simplificação
-    const ratio = frequency / maxFrequency;
-    const red = Math.min(255, Math.floor(255 * ratio));
-    const green = 255 - red;
-    const blue = 255 - red;
-    return `rgb(255, ${green}, ${blue})`;
+    let ratio = 0
+    let green = 0
+    let blue = 0
+    let red = 0
+    let yellow = 0
+    if (frequency <= 500) {
+        ratio = frequency / 500
+        yellow = Math.min(255, Math.floor(255 * ratio))
+        green = 255
+        blue = 255 - yellow
+    } else {
+        ratio = (frequency - 500) / 500
+        red = Math.min(255, Math.floor(255 * ratio))
+        green = 255 - red
+    }
+    return `rgb(255, ${green}, ${blue})`
 }
 
 // Função principal para atualizar a cor das teclas após carregar o JSON
--- scriptPython/main.py
+++ scriptPython/main.py
@@ -4,41 +4,91 @@ import json
 import os
 import datetime
 
-#Dicionário para armazenar contagem de teclas
+# Dicionário para armazenar contagem de teclas
 key_count = defaultdict(int)
+
+# Teclas atualmente pressionadas
+pressed_keys = set()
 
 # Carregar o arquivo JSON existente (se existir)
 if os.path.exists('key_count.json'):
     with open('key_count.json', 'r') as file:
         key_count.update(json.load(file))
 
-#Identificar teclado numérico
+# Identificar teclado numérico
 num_pad_keys = {
-    "<96>": 'num_0',
-    "<97>": 'num_1',
-    "<98>": 'num_2',
-    "<99>": 'num_3',
-    "<100>": 'num_4',
-    "<101>": 'num_5',
-    "<102>": 'num_6',
-    "<103>": 'num_7',
-    "<104>": 'num_8',
-    "<105>": 'num_9',
-    "<110>": 'num_decimal',
-    "<107>": 'num_add',
-    "<109>": 'num_subtract',
-    "<106>": 'num_multiply',
-    "<111>": 'num_divide',
+    96: 'num_0',
+    97: 'num_1',
+    98: 'num_2',
+    99: 'num_3',
+    100: 'num_4',
+    101: 'num_5',
+    102: 'num_6',
+    103: 'num_7',
+    104: 'num_8',
+    105: 'num_9',
+    110: 'num_decimal',
+    107: 'num_add',
+    109: 'num_subtract',
+    106: 'num_multiply',
+    111: 'num_divide',
+    194: 'num_dot',
 }
 
-
-# Função chamada quando uma tecla é pressionada
-def on_press(key):
+def getKey(key):
     try:
         # Convertendo a tecla para string
-        key_str = key.char
+        if hasattr(key, 'vk') and key.vk in num_pad_keys:
+            key_str = num_pad_keys[key.vk]
+        else:
+            key_str = key.char
+        # Verifica se retorna None, quando utiliza o teclado numérico
         if key_str is not None:
-            pass
+            # Atribuir a tecla normal quando pressionada junto ao Shift
+            if key_str == '"':
+                key_str = "'"
+            elif key_str == "!":
+                key_str = "1"
+            elif key_str == "@":
+                key_str = "2"
+            elif key_str == "#":
+                key_str = "3"
+            elif key_str == "$":
+                key_str = "4"
+            elif key_str == "%":
+                key_str = "5"
+            elif key_str == "¨":
+                key_str = "6"
+            elif key_str == "&":
+                key_str = "7"
+            elif key_str == "*":
+                key_str = "8"
+            elif key_str == "(":
+                key_str = "9"
+            elif key_str == ")":
+                key_str = "0"
+            elif key_str == "_":
+                key_str = "-"
+            elif key_str == "+":
+                key_str = "="
+            elif key_str == "`":
+                key_str = "´"
+            elif key_str == "{":
+                key_str = "["
+            elif key_str == "^":
+                key_str = "~"
+            elif key_str == "}":
+                key_str = "]"
+            elif key_str == "|":
+                key_str = "\\"
+            elif key_str == "<":
+                key_str = ","
+            elif key_str == ">":
+                key_str = "."
+            elif key_str == ":":
+                key_str = ";"
+            elif key_str == "?":
+                key_str = "/"
         else:
             key_str = num_pad_keys[f'{key}']
     except AttributeError:
@@ -47,20 +97,45 @@ def on_press(key):
         else:
             # Tratando teclas especiais (e.g., Shift, Ctrl)
             key_str = str(key)
+    return key_str
 
-    # Incrementando a contagem da tecla
-    key_count[key_str] += 1
 
-    # Salvando o resultado em um arquivo JSON a cada tecla pressionada (opcional)
-    with open('key_count.json', 'w') as file:
-        json.dump(key_count, file, ensure_ascii=False, indent=4)
+# Função chamada quando uma tecla é pressionada
+def on_press(key):
 
-    # Exibindo a tecla e sua contagem (opcional)
-    print(f"{key_str}: {key_count[key_str]}")
+    key_str = getKey(key)
 
+    # Verifica se a tecla está sendo pressionada, evitando registrar várias vezes
+    if key_str not in pressed_keys:
+        pressed_keys.add(key_str)
+
+        # Incrementando a contagem da tecla
+        key_count[key_str] += 1
+
+        # Salvando o resultado em um arquivo JSON a cada tecla pressionada (opcional)
+        with open('key_count.json', 'w') as file:
+            json.dump(key_count, file, ensure_ascii=False, indent=4)
+
+        # Exibindo a tecla e sua contagem (opcional)
+        print(f"{key_str}: {key_count[key_str]}")
+
+    #print(pressed_keys)
 
 # Função chamada quando uma tecla é liberada
 def on_release(key):
+
+    key_str = getKey(key)
+
+    # Remove a tecla da lista de teclas pressionadas
+    if key_str in pressed_keys:
+        pressed_keys.remove(key_str)
+
+    if key in pressed_keys:
+        pressed_keys.remove(key)
+
+    if str(key) in pressed_keys:
+        pressed_keys.remove(str(key))
+
     if key == keyboard.Key.f7:
         # Parar o listener
         return False
@@ -68,41 +143,7 @@ def on_release(key):
 
 # Configurando o listener para eventos do teclado
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
-'''
-
-count_cedilha = 0
-count_numerico = 0
-log_file = 'log.txt'
-
-def on_press(key):
-    global count_cedilha
-    global count_numerico
-    try:
-        with open(log_file, 'a') as f:
-            f.write(f'{key.char} {keyboard.Key} {key} - {datetime.datetime.now()}\n')
-
-        if key.char == 'ç':
-            count_cedilha += 1
-            print(f'Tecla ç pressionada {count_cedilha} vezes')
-        if key.char in [';', '*', '-', '+', '.', 'None']:
-            count_numerico += 1
-            print(f'Teclado numérico utilizado {count_numerico} vezes')
-
-
-    except AttributeError:
-        with open(log_file, 'a') as f:
-            f.write(f'{key} - {datetime.datetime.now()}\n')
-
-def on_release(key):
-    if key == keyboard.Key.f7:
-        return False
-
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     try:
         listener.join()
     except:
-        if key == keyboard.Key.f7:
-            print(f'Tecla F7 pressionada. Saindo da aplicação...')
-            exit()
-        print(f'Tecla inválida pressionada...')'''
+        exit()
